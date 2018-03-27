ffbd764f6491e2f13018ccef5f4a88a65d4006a7
Joaquín Siabra <jasiabra@profile.es>
2018-03-27 15:10:20 +0200

    Añadido dashboard principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
-=======
 import { AppRoutingModule } from './/app-routing.module';
->>>>>>> feature/routing
+
 
 @NgModule({
   declarations: [
