cd603fc4b52a3f1071dafeabbcf0fc161a655925
Joaquín Siabra <jasiabra@profile.es>
2018-04-02 11:00:19 +0200

    Errores y log del servicio

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,7 +6,7 @@ import { of } from 'rxjs/observable/of';
 import { Hero } from './hero';
 //import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
-
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
@@ -17,11 +17,14 @@ private heroesUrl = 'api/heroes';  // URL to web api
 	  private http: HttpClient,
 	  private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-      return this.http.get<Hero[]>(this.heroesUrl)
-  }
+	  
+	getHeroes (): Observable<Hero[]> {
+	  return this.http.get<Hero[]>(this.heroesUrl)
+		.pipe(
+		  tap(heroes => this.log(`fetched heroes`)),
+		  catchError(this.handleError('getHeroes', []))
+		);
+	}
 
   getHero(id: number): Observable<Hero> {
     // Todo: send the message _after_ fetching the hero
@@ -33,5 +36,26 @@ private heroesUrl = 'api/heroes';  // URL to web api
 	private log(message: string) {
 	  this.messageService.add('HeroService: ' + message);
 	}
+  
+	  /**
+	 * Handle Http operation that failed.
+	 * Let the app continue.
+	 * @param operation - name of the operation that failed
+	 * @param result - optional value to return as the observable result
+	 */
+	private handleError<T> (operation = 'operation', result?: T) {
+	  return (error: any): Observable<T> => {
+
+		// TODO: send the error to remote logging infrastructure
+		console.error(error); // log to console instead
+
+		// TODO: better job of transforming error for user consumption
+		this.log(`${operation} failed: ${error.message}`);
+
+		// Let the app keep running by returning an empty result.
+		return of(result as T);
+	  };
+	  
+}
   
 }
